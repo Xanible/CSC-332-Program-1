293fce1ac7626030b9eb1f98856d54cd52eaaa17
spy998 <spy998lper@gmail.com>
2016-09-27 14:10:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <time.h>
 
+void randomizeArray(int array[], int length)
+{
+	srand((unsigned)time(NULL));			// Seed random number generator. 
+
+	int i = 0;
+	do
+	{
+		array[i] = rand() % 100000 + 1;		// A random number in the range of 1 to 100,000 is assigned.
+		i++;
+	} while (i < length);
+}
 
 int main(int argc, char** argv) {
-		clock_t startTime, endTime;
+	clock_t startTime, endTime;
 	// Randomize values in array.
 	randomizeArray(array, length);
 
