edd797d7a9029af4d31fbbeb763eba69101ebd37
spy998 <spy998lper@gmail.com>
2016-09-27 14:09:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <time.h>
 
 
 int main(int argc, char** argv) {
+		clock_t startTime, endTime;
+	// Randomize values in array.
+	randomizeArray(array, length);
+
+	// Time array sort.
+	startTime = clock();			// Get starting time.
+	selectionSort(array, length);	// Sort array.
+	endTime = clock();				// Get ending time.
 	return 0;
 }
